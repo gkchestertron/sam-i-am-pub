029f5e16293550b229cba3d0e00f32470220d14b
John Fellman <jfellman@jfellman-q8jhd4.local>
2019-04-09 17:43:12 -0700

    init commit

---
+++ hello-world/app.js
@@ -0,0 +1,50 @@
+const axios = require('axios')
+const url = 'http://checkip.amazonaws.com/';
+let response;
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+exports.lambdaHandler = async (event, context) => {
+  let ip;
+  let response;
+
+  try {
+    response = await axios.get(url)
+    ip = response.data.replace("\n","")
+    response = {
+      'statusCode': 200,
+      'body': JSON.stringify({
+        message: 'hello world, your ip is: ' + ip,
+        // location: ret.data.trim()
+      })
+    }
+  } catch(err) {
+    return { error: err }
+  }
+
+  // try {
+  //   // const ret = await axios(url);
+  //   response = {
+  //     'statusCode': 200,
+  //     'body': JSON.stringify({
+  //       message: 'hello world, your ip is: ' + ip,
+  //       // location: ret.data.trim()
+  //     })
+  //   }
+  // } catch (err) {
+  //   console.log(err);
+  //   return err;
+  // }
+
+  return response
+};
